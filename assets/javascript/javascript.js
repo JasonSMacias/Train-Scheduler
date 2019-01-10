df5e629741931e3ef55a6e2ade984187e26348a3
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-01-09 21:02:29 -0500

    readme, boilerplate, initialize firebase

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAd5IL94KHV0qC9BuiDN23_l5TNuPg3d2w",
+    authDomain: "classprojects-d8b8c.firebaseapp.com",
+    databaseURL: "https://classprojects-d8b8c.firebaseio.com",
+    projectId: "classprojects-d8b8c",
+    storageBucket: "classprojects-d8b8c.appspot.com",
+    messagingSenderId: "349136664077"
+  };
+  firebase.initializeApp(config);
+</script>
