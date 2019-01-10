c44bb5439afc14c99d36a7ce65c421fbe369eddf
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-01-09 22:32:47 -0500

    Added javascript form capture and submit to firebase

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -12,3 +12,27 @@ firebase.initializeApp(config);
 // variable to hold firebase db
 var database = firebase.database();
 
+// Submit button on click to add train object
+$("#submit-button").on("click", function(event) {
+  event.preventDefault();
+
+  // Grabs user input
+  var trainName = $("#train-name-input").val().trim();
+  var trainDestination = $("#destination-input").val().trim();
+  var trainStart = $("#first-train-time-input").val().trim();
+  var trainFrequency = $("#frequency-input").val().trim();
+
+  // Creates local "temporary" object for holding employee data
+  var newTrain = {
+    name: trainName,
+    destination: trainDestination,
+    startTime: trainStart,
+    frequency: trainFrequency
+  };
+
+  // pushes new train object to firebase & console logs
+  database.ref().push(newTrain);
+  console.log(newTrain);
+
+
+});
